293e66423dca84124227ef32e9739e094a8abf05
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2020-06-01 00:53:57 -0500

    Create guildMemberRemove.js

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const prettyMs = require("pretty-ms");
+module.exports = {
+  name: "guildMemberRemove",
+  disabled: false,
+  run: async (client,member) => {
+    //Code here
+    
+  }
+}
