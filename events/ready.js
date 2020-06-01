e2d3e4175138f4817dfebd08d17c5019e51c0783
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2020-06-01 01:02:27 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,16 @@ module.exports = {
   name: "ready",
   run: async (client) => {
     console.log(`✔️ Bot successfully logged as ${client.user.tag} (${client.user.id})`)
+    runDefaults(client);
     //Code here
+    
   }
+}
+function runDefaults(client){
+  client.prefixes.push(`<@${client.user.id}>`);
+  client.prefixes.push(`<@!${client.user.id}>`);
+  client.mentions = [`<@${client.user.id}>`,`<@!${client.user.id}>`]
+  client.users.fetch(client.developerid).then(u=>{
+    client.developer = u;
+  }).catch(e=>{});
 }
