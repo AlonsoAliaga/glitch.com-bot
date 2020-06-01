fc7ca9cfffe0e1cf0af94410024ea3ef57e4dbbb
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2020-06-01 00:45:01 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -24,7 +24,7 @@ module.exports = {
     if (!message.member) message.member = await message.guild.members.fetch(message);
     
     //This is the command handler.
-    if(message.content && client.prefixes.some(p=>message.content.toLowerCase().startsWith(p))) {
+    if (message.content && client.prefixes.some(p=>message.content.toLowerCase().startsWith(p))) {
       let prefixUsed = client.prefixes.find(p=>message.content.toLowerCase().startsWith(p));
       let args = message.content.slice(prefixUsed.length).trim().split(' ');
       let cmd = args.shift().toLowerCase();
@@ -34,16 +34,17 @@ module.exports = {
       } else if (client.aliases.has(cmd)) {
         command = client.commands.get(client.aliases.get(cmd));
       }else return;
-      if (command.developer && message.author.id !== client.developerid) return;
-      if(command.cooldown && command.cooldownCollection){
-        if(command.cooldownCollection.has(message.author.id)){
-          if (deleteOnCooldown) message.delete({timeout:5000}).catch(e=>{})
+      if (command.owner && message.author.id !== message.guild.ownerID) return; //Only for guild's owner.
+      if (command.developer && message.author.id !== client.developerid) return; //Only for bot's developer.
+      if (command.cooldown && command.cooldownCollection) { //Cooldown.
+        if (command.cooldownCollection.has(message.author.id)) {
+          if (command.deleteOnCooldown) message.delete({timeout:5000}).catch(e=>{})
           return message.channel.send(`${client.emoji.cross} You are running commands too quickly. **Chill out a bit and try in ${prettyMs(command.cooldownCollection.get(message.author.id) - Date.now())}!**`).then(m=>{
               if (command.deleteOnCooldown) m.delete({timeout:5000}).catch(e=>{})
             }).catch(e=>{})
         }
         command.cooldownCollection.set(message.author.id,Date.now() + command.cooldown)
-        setTimeout(()=>{
+        setTimeout(() => {
           command.cooldownCollection.delete(message.author.id)
         },command.cooldown);
       }
