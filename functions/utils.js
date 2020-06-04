663d7cca26af0868887e09100a29e5b144ee6dcc
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2020-06-03 20:55:36 -0500

    Update utils.js

--- functions/utils.js
+++ functions/utils.js
@@ -2,7 +2,7 @@
 //Simply call " client.utils.yourMethod(parameters); "
 //There are some useful examples below that will help you with your bot. (Recommended to keep them)
 module.exports.capitalize = (string = "",allWords = false) => {
-  if(string.length === 0) return "";
+  if (string.length === 0) return "";
   return !allWords?`${string.substring(0,1).toUpperCase()}${string.slice(1).toLowerCase()}`:string.split(" ").map(w=>w.length === 0?
     w:`${w.substring(0,1).toUpperCase()}${w.slice(1).toLowerCase()}`).join(" ")
 }
@@ -14,7 +14,7 @@ module.exports.getMemberInfo = (client,member) => {
 }
 module.exports.getMemberData = (client,member) => {
   let memberData = this.getMemberInfo(client,member);
-  if(!memberData){
+  if (!memberData){
     this.registerMember(client,member);
     console.log(`📚 Auto registering ${member.user.tag} (${member.id}) in guild '${member.guild.name}' (${member.guild.id})`)
     memberData = this.getMemberInfo(client,member);
@@ -38,7 +38,7 @@ const PERIOD = {
 }
 module.exports.toMs = (duration = "") => {
   let matchs = duration.match(/[0-9]+([smhdwy])/g);
-  if(!matchs || matchs.length === 0) return undefined;
+  if (!matchs || matchs.length === 0) return undefined;
   return matchs.reduce((acc, value) => acc += value.replace(/(ms|[smhdwy])/g,'') * PERIOD[value.slice(-1)],0,)
 }
 module.exports.getMember = async (message, toCheck, defaultAuthor = false, includeNames = false) => {
@@ -46,7 +46,7 @@ module.exports.getMember = async (message, toCheck, defaultAuthor = false, inclu
   let possibleId = toCheck.replace(/[^0-9]/g,"")
   let member = message.guild.members.cache.get(possibleId);
   if (!member) {
-    if(possibleId.length === 18){
+    if (possibleId.length === 18){
       try {
         return await message.guild.members.fetch(possibleId);
       } catch(e) {}
